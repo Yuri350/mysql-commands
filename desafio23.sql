97357261133ac78adda4330b880d041cd9d9787d
Yuri Villanueva <yuri.350@hotmail.com>
2022-07-19 12:37:52 -0300

    20 ao 24 ok

---
+++ desafio23.sql
@@ -0,0 +1,3 @@
+UPDATE order_details WHERE unit_price > 10000 SET discount = 30;
+
+--  Atualize os dados da coluna 'discount' da tabela 'order_details' para 30, onde o valor na coluna 'unit_price' seja menor que 10.0000
